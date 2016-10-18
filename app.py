c28b1b136e4960bd50c59cbce84dcdbb84b3e8dd
Xyllan <doruketc@gmail.com>
2016-10-18 19:54:17 +0300

    Added random document query and sentiment/relevance submission

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from base64 import b64encode
 from uuid import uuid4
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 import random
 
 app = Flask(__name__)
@@ -23,7 +24,13 @@ def get_doc_find_query(username = None, annotated = False):
 def get_document_count(query):
 	return documents.find(query).count()
 
-def get_document():
+def update_document(document):
+	return documents.replace_one({'_id':document['_id']}, document)
+
+def get_document(doc_id):
+	return documents.find_one({"_id":doc_id})
+
+def get_random_document():
 	try:
 		q = get_doc_find_query(session['username'], not session['has_unused'])
 		cnt = get_document_count(q)
@@ -33,16 +40,22 @@ def get_document():
 	except ValueError:
 		if session['has_unused']: # There are no articles left with 0 annotations
 			session['has_unused'] = False
-			return get_document()
+			return get_random_document()
 		else:
 			return None # There are no articles left that the user hasn't annotated
+
+def add_document_to_session(session, document):
+	session['document'] = {'_id':str(document['_id']),'text':document['text']}
 
 @app.route('/')
 def index():
 	if 'uuid' not in session:
 		session['uuid'] = str(uuid4())
 	if 'username' in session:
-		return render_template('index.html', username = session['username'], document = get_document())
+		if not 'document' in session:
+			d = get_random_document()
+			add_document_to_session(session, d)
+		return render_template('index.html', username = session['username'], text = session['document']['text'])
 	else:
 		return render_template('index.html')
 
@@ -55,6 +68,33 @@ def set_username():
 		return jsonify(result="refresh")
 	else:
 		return jsonify(result="Username changed to "+request.form['username'])
+
+@app.route('/set_sentiment', methods=['POST'])
+def set_sentiment():
+	try:
+		sentiment_score = int(request.form['sentiment'])
+		relevance_score = int(request.form['relevance'])
+		assert(sentiment_score in [-1,0,1] and relevance_score in [-1,0,1]) # Defense against unknown queries
+
+		d = get_document(ObjectId(session['document']['_id'])) # Get a fresh copy of the document
+		sent = d['sentiment'] if 'sentiment' in d else {'num_scored':0, 'sentiments':[]}
+		sent['num_scored']+=1
+		sent['sentiments'].append({'user': {'username':session['username'],'session_id':session['uuid']},
+			'sentiment': sentiment_score,
+			'relevance': relevance_score,
+			'phrases':[]}) # TODO phrases
+		d['sentiment'] = sent
+		res = update_document(d)
+		if res.matched_count > 0:
+			# Update success, get new document for the user
+			add_document_to_session(session, get_random_document())
+			return jsonify(error = 0, text = session['document']['text'])
+		else:
+			# Update failed, display error message
+			return jsonify(error = 1)
+	except:
+		# Wrong info supplied with query
+		return jsonify(error = 2)
 
 """
 @app.cli.command('init')
